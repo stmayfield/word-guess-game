e651e77a98749246875c20d382e0ef12db6ec0bb
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-04-18 20:02:16 -0700

    Added forLoop() pseudocode in game.js to convey program's intended function; Added primary() to call index if/else statement and keydown event listener for each index loop; added comments for several code blocks and functions to describe programs intended function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,15 @@
-//Hide all HTML Elements, Unhide board (extract to mutltiple functions, one for hide, one for unhide, one for game start)
-const board = document.querySelector(".board");
+//Hide all HTML Elements, Unhide board (extract to mutltiple functions, one for hide, one for unhide, one for game start)//
+const boardClass = document.querySelector(".board");
+const boardId = document.querySelector("#board");
 const keyPress = document.getElementById("key-press");
 const score = document.getElementById("score");
+const remainGuess = document.getElementById("score");
+const span = document.getElementById("span");
+var guessEl = document.getElementById("guess");
+
 let leftGuess = 5
 
-
-
+//Functions for start of the game: beginHide() hides <h1 id="key-press"> and displayBoard() sets <p class="board"> to visible//
 function beginHide() {
     addEventListener("click", function () {
         if (keyPress.style.visibility === "visible") {
@@ -17,99 +21,206 @@ function beginHide() {
 }
 function displayBoard() {
     addEventListener("click", function () {
-        if (board.style.visibility === "hidden") {
-            board.style.visibility = "visible";
+        if (boardClass.style.visibility === "hidden") {
+            boardClass.style.visibility = "visible";
+            boardId.style.visibility = "visible";
         } else {
-            board.style.visibility = "visible";
+            boardClass.style.visibility = "visible";
         }
     })
 }
 
+//Functions executed//
 beginHide();
 displayBoard();
 
+//Array containing all words//
+const words = ["cortana", "pelican", "gravemind", "monitor", "covenant"]
 
-
-
-//Array containing all words
-const words = ["wordone", "wordnumbertwo", "wordthree", "thelongestwordhere"]
-
-// Variables declared for each index
-const a = words[0]
-
-//Split chars in word into an array
-let aChar = a.split("");
-
-//initialize first word
-index = 0
-
-
-// for (var i = 0; i < words.length; i++) {
-//     if () {
-
-//     } else {
-//         index++;
-//     }
-
-//     // function initializing game
-// }
-
-// displaying blanks to word
+// Variables declared for each index//, //Split chars in word into an array//, //Displaying blanks to word //
+let index = 0
 let wordIndex = words[index]
 let chars = wordIndex.split("");
 let blank = [];
 let placeholder = " "
 let undscrString = "_"
 
-// Creates an array to access and display each character in word
+// Creates an array to access and display a "_" + " " for each character in word//
 for (var i = 0; i < chars.length; i++) {
     if (i < chars.length) {
-        var span = document.createElement("span");
-        span.textContent = placeholder + undscrString;
-        board.append(span);
+        var spanEl = document.createElement("span");
+        spanEl.textContent = placeholder + undscrString;
+        boardClass.append(spanEl);
         blank.push(undscrString);
     } else {
         break;
     }
 }
 
+function forLoop() {
+    /* The following code was intended to utilize a for loop to cycle through each value in the array for words = []
+    
+    //For loop: initialize i; continue for everytime i is less than the length of words = [] (); add 1 to i//
+    for (var i = 0; i < words.length; i++) {
+    //if index is less than words.length, then run the following code block   //     
+        if () {
+            primary();
+            //Add 1 to index then loop to beginning//
+            index++;
+        } else {
+            break;
+        }}
+    */
+}
 
+//Declare DOM variable for primary() //
+var spanNodes = document.body.firstElementChild.childNodes[3].childNodes;
 
-function onKeyPress() {
-    document.onkeyup = function (event) {
-        var userGuess = event.key;
-        if (userGuess === "w") {
-            document.body.firstElementChild.lastElementChild.childNodes[2].textContent = "w";
-        } else if (userGuess === "o") {
-            document.body.firstElementChild.lastElementChild.childNodes[3].textContent = "o";
-            document.body.firstElementChild.lastElementChild.childNodes[6].textContent = "o";
-        } else if (userGuess === "r") {
-            document.body.firstElementChild.lastElementChild.childNodes[4].textContent = "r";
-        } else if (userGuess === "d") {
-            document.body.firstElementChild.lastElementChild.childNodes[5].textContent = "d";
-        } else if (userGuess === "n") {
-            document.body.firstElementChild.lastElementChild.childNodes[7].textContent = "n";
-        } else if (userGuess === "e") {
-            document.body.firstElementChild.lastElementChild.childNodes[8].textContent = "e";
-        } else if (userGuess !== "w", "o", "r", "d", "n", "e") {
-            leftGuess = leftGuess - 1
-        }
+function loopPlusTwo() {
+    for (var i = 0; i < chars.length; i++) {
+        spanNodes = spanNodes[i + 2];
+        return spanNodes;
     }
 }
 
+//Declares function for else/if statements//
+function elseIfFunc(letter, iii) {
+    (alpha === letter)
+    spanNodes[iii + 2].textContent = letter;
+}
 
-onKeyPress();
+function letter(wordChar) {
+    alpha === wordchar;
+    spanInit.textContent = wordChar;
+}
 
+//Primary Function called in the for loop on line 63//
+function primary() {
 
-score.textContent = leftGuess;
+    //Else/If Code block for each value in words array//
+    if (index === 0) {
 
+        //Add event listener to document to read user keydown input, convert to a lower case character, and output the resulting character into a corresponding  //
+        document.addEventListener("keydown", function (event) {
+            var key = event.key.toLowerCase();
+            var alphaNum = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+            var alpha = alphaNum.includes(key);
+            if (alpha !== chars) {
+                leftGuess = leftGuess--;
+            } else if (alpha === "c") {
+                elseIfFunc("c", 0);
+            } else if (alpha === "o") {
+                elseIfFunc("o", 1);
+            } else if (alpha === "r") {
+                elseIfFunc("r", 2);
+            } else if (alpha === "t") {
+                elseIfFunc("t", 3);
+            } else if (alpha === "a") {
+                elseIfFunc("a", 4);
+            } else if (alpha === "n") {
+                elseIfFunc("n", 5);
+            } else if (alpha === "a") {
+                elseIfFunc("a", 6);
+            }
+        });
+    } else if (index === 1) {
+        document.addEventListener("keydown", function (event) {
+            var key = event.key.toLowerCase();
+            var alphaNum = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+            var alpha = alphaNum.includes(key);
+            if (alpha !== chars) {
+                leftGuess = leftGuess--;
+            } else if (alpha === "p") {
+                elseIfFunc("p", 0);
+            } else if (alpha === "e") {
+                elseIfFunc("e", 1);
+            } else if (alpha === "l") {
+                elseIfFunc("l", 2);
+            } else if (alpha === "i") {
+                elseIfFunc("i", 3);
+            } else if (alpha === "c") {
+                elseIfFunc("c", 4);
+            } else if (alpha === "a") {
+                elseIfFunc("a", 5);
+            } else if (alpha === "n") {
+                elseIfFunc("n", 6);
+            }
+        });
+    } else if (index === 2) {
+        document.addEventListener("keydown", function (event) {
+            var key = event.key.toLowerCase();
+            var alphaNum = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+            var alpha = alphaNum.includes(key);
+            if (alpha !== chars) {
+                leftGuess = leftGuess--;
+            } else if (alpha === "g") {
+                elseIfFunc("g", 0);
+            } else if (alpha === "r") {
+                elseIfFunc("r", 1);
+            } else if (alpha === "a") {
+                elseIfFunc("a", 2);
+            } else if (alpha === "v") {
+                elseIfFunc("v", 3);
+            } else if (alpha === "e") {
+                elseIfFunc("e", 4);
+            } else if (alpha === "m") {
+                elseIfFunc("m", 5);
+            } else if (alpha === "i") {
+                elseIfFunc("i", 6);
+            } else if (alpha === "n") {
+                elseIfFunc("n", 7);
+            } else if (alpha === "d") {
+                elseIfFunc("d", 8);
+            }
+        });
+    } else if (index === 3) {
+        document.addEventListener("keydown", function (event) {
+            var key = event.key.toLowerCase();
+            var alphaNum = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+            var alpha = alphaNum.includes(key);
+            if (alpha !== chars) {
+                leftGuess = leftGuess--;
+            } else if (alpha === "m") {
+                elseIfFunc("m", 0);
+            } else if (alpha === "o") {
+                elseIfFunc("o", 1);
+            } else if (alpha === "n") {
+                elseIfFunc("n", 2);
+            } else if (alpha === "i") {
+                elseIfFunc("i", 3);
+            } else if (alpha === "t") {
+                elseIfFunc("t", 4);
+            } else if (alpha === "o") {
+                elseIfFunc("o", 5);
+            } else if (alpha === "r") {
+                elseIfFunc("r", 6);
+            }
+        });
+    } else if (index === 4) {
+        document.addEventListener("keydown", function (event) {
+            var key = event.key.toLowerCase();
+            var alphaNum = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+            var alpha = alphaNum.includes(key);
+            if (alpha !== chars) {
+                leftGuess = leftGuess--;
+            } else if (alpha === "c") {
+                elseIfFunc("c", 0);
+            } else if (alpha === "o") {
+                elseIfFunc("o", 1);
+            } else if (alpha === "v") {
+                elseIfFunc("v", 2);
+            } else if (alpha === "e") {
+                elseIfFunc("e", 3);
+            } else if (alpha === "n") {
+                elseIfFunc("n", 4);
+            } else if (alpha === "a") {
+                elseIfFunc("a", 5);
+            } else if (alpha === "n") {
+                elseIfFunc("n", 6);
+            } else if (alpha === "t") {
+                elseIfFunc("t", 7);
+            }
+        });
+    };
 
-// textAreaEl.addEventListener("keydown", function (event) {
-//     var key = event.key.toLowerCase();
-//     var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-//     if (alphabetNumericCharacters.includes(key)) {
-//         elements.forEach(function (element) {
-//             element.textContent += event.key;
-//         });
-//     }
-// });
+}
